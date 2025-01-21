7015570c4a3b8fc844ed62a7142bb1ab73b121fe
sethg <sethg@geographika.co.uk>
2025-01-21 09:34:56 +0100

    upload-wheels

--- scripts/configure-mapserver.ps1
+++ scripts/configure-mapserver.ps1
@@ -35,6 +35,13 @@ $PROJECT_BINARY_DIR="$ROOT_FOLDER/build"
 $SWIG_PYTHON_INTERPRETER_NO_DEBUG=1
 $PYMAPSCRIPT_ANNOTATIONS=1
 
+if ($PYTHON_VERSION -eq "3.13") {
+    $sourcePath = Join-Path $SDK_BIN "sqlite3.dll"
+    $targetPath = "c:\hostedtoolcache\windows\python\3.13.1\x64\DLLs"
+    Copy-Item -Path $sourcePath -Destination $targetPath -Force
+    Write-Host "sqlite3.dll successfully copied to $targetPath"
+}
+
 cmake -G "$VS_VER" -A "x64" "$ROOT_FOLDER/mapserver" `
 -DCMAKE_BUILD_TYPE=Release `
 -DCMAKE_PREFIX_PATH="$SDK_PREFIX" `
@@ -83,15 +90,8 @@ cmake --build . --target pythonmapscript-wheel --config Release
 
 echo "Using $PYTHON_VERSION"
 
-if ($PYTHON_VERSION -eq "3.13") {
-    $sourcePath = Join-Path $SDK_BIN "sqlite3.dll"
-    $targetPath = "c:\hostedtoolcache\windows\python\3.13.1\x64\DLLs"
-    Copy-Item -Path $sourcePath -Destination $targetPath -Force
-    Write-Host "sqlite3.dll successfully copied to $targetPath"
-}
-
 # make sdist for one version of Python as they are identical
-if ($PYTHON_VERSION -eq "3.12")
+if ($PYTHON_VERSION -eq "3.13")
 {
     echo "Creating sdist..."
     cd "$ROOT_FOLDER/build/src/mapscript/python/Release"
